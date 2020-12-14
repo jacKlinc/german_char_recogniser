d8b0cc22511d98dd5209eaa56570819290194bb3
jacklinc <jackharding321@gmail.com>
2020-12-14 10:49:30 +0100

    - Added exported model - Added streamlit dash

---
+++ src/src_streamlit-dash.py
@@ -0,0 +1,51 @@
+from fastai.learner import load_learner
+from fastai.vision.core import PILImage
+
+import streamlit as st
+from PIL import Image
+
+def load_image(image):
+    '''
+    Pass    BytesIO image
+    Return  PILImage object
+    '''
+    return Image.open(image)
+
+def predict_img(img):
+    '''
+    Pass    PILImage object
+    Return  prediction[str], prediction_idx[int], probability[tensor]
+    '''
+    if img is not None:
+        return learner_inf.predict(pil_img)
+
+'## German Character Recogniser'
+"Here's the [GitHub](https://github.com/jacKlinc/german_char_recogniser) repo"
+'Upload a picture of a handwritten A/B'
+learner_inf = load_learner('./export.pkl')
+
+# Upload
+pic = st.file_uploader("Upload Image")
+
+'Click Classify to find whether it\'s an A or a B'
+
+probs = []
+pred_idx = 1
+pred = 'n/a'
+
+# Display image
+if pic is not None:
+    img = load_image(pic)
+    st.image(img)
+    
+    
+    # Parse image
+    pil_img = PILImage.create(pic)
+
+    # Predict category
+    pred,pred_idx,probs = predict_img(pil_img)
+
+# Classify
+if st.button('Classify'):
+    'Prediction: ', pred
+    'Probability: ', str(round(probs[pred_idx].item(), 5))
